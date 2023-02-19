9a69c85ffaff5712bdc81fdb3c6449c71559e4d0
RafaRato <rafaelcteixeira1@hotmail.com>
2023-02-19 20:23:25 -0300

    implementation of operation and api responses od consultaEndereco

--- src/main/java/com/example/ConsultaEndViaCEP/consulta/controller/ConsultaEnderecoController.java
+++ src/main/java/com/example/ConsultaEndViaCEP/consulta/controller/ConsultaEnderecoController.java
@@ -5,6 +5,10 @@ import com.example.ConsultaEndViaCEP.consulta.model.Message;
 import com.example.ConsultaEndViaCEP.consulta.service.ConsultaEnderecoService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
@@ -19,7 +23,21 @@ public class ConsultaEnderecoController {
     @Autowired
     public ConsultaEnderecoService consultaEnderecoService;
 
+    /**
+     * Consulta o endereço correspondente ao CEP informado.
+     *
+     * @param request Objeto que representa a requisição de consulta de endereço.
+     * @return O endereço correspondente ao CEP informado.
+     *
+     * @tag Consulta de endereço
+     */
     @PostMapping
+    @Operation(summary = "Consulta o endereço correspondente ao CEP informado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Endereço correspondente ao CEP informado encontrado"),
+            @ApiResponse(responseCode = "400", description = "Não foi possível encontrar o endereço correspondente ao CEP informado ou CEP inválido",
+                    content = @Content(mediaType = "application/json"))
+    })
     public ResponseEntity<String> consultaEndereco(@RequestBody ConsultaEnderecoRequest request) throws JsonProcessingException {
         String cep = request.getCep();
 
